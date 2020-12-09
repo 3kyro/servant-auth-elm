d0b753a082c112f146bfd2d714693b21bc8ea775
3kyro <k.papachrisanthou@gmail.com>
2020-12-09 10:39:48 +0100

    remove cli argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,6 @@
 module Main where
 
-import Lib
-import System.Environment (getArgs)
+import Lib ( mainWithCookies )
 
 main :: IO ()
-main = do
-  args <- getArgs
-  let usage = "Usage: readme (JWT|Cookie)"
-  case args of
-    ["Cookie"] -> mainWithCookies
-    e -> putStrLn $ "Arguments: \"" ++ unwords e ++ "\" not understood\n" ++ usage
+main = mainWithCookies
