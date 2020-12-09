402a456b595837c66869f7137ad6ef2e289bf4a7
3kyro <k.papachrisanthou@gmail.com>
2020-12-09 10:10:53 +0100

    elm related gitignore

--- frontend/init.js
+++
@@ -1,7 +0,0 @@
-let token = (function() {
-    r = document.cookie.match(new RegExp('XSRF-TOKEN=([^;]+)'))
-    if (r) return r[1];)();
-Elm.App.init(
-    { node: document.getElementById("app")
-    , flags : token
-    });
